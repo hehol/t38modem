ec6165d58b485b3229e2c6ccc2a7f873ad6b9b47
craigs <craigs>
2002-01-01 23:41:30 +0000

    Updated to version 1.1

--- version.h
+++ version.h
@@ -1,12 +1,12 @@
 
-#ifndef _pty_VERSION_H
-#define _pty_VERSION_H
+#ifndef _T38M_VERSION_H
+#define _T38M_VERSION_H
 
-#define MAJOR_VERSION 1
+#define MAJOR_VERSION 0
 #define MINOR_VERSION 0
 #define BUILD_TYPE    ReleaseCode
 #define BUILD_NUMBER 0
 
 
-#endif  // _pty_VERSION_H
+#endif  // _T38M_VERSION_H
 
