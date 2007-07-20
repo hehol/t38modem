5761343e29b2c5ffe4e036e42159cc8836bc8cb3
vfrolov <vfrolov>
2007-07-20 14:26:40 +0000

    Initial revision

---
+++ opal/opalutils.cxx
@@ -0,0 +1,58 @@
+/*
+ * opalutils.cxx
+ *
+ * T38FAX Pseudo Modem
+ *
+ * Copyright (c) 2007 Vyacheslav Frolov
+ *
+ * Open H323 Project
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open H323 Library.
+ *
+ * The Initial Developer of the Original Code is Vyacheslav Frolov
+ *
+ * Contributor(s):
+ *
+ * $Log: opalutils.cxx,v $
+ * Revision 1.1  2007-07-20 14:26:40  vfrolov
+ * Initial revision
+ *
+ * Revision 1.1  2007/07/20 14:26:40  vfrolov
+ * Initial revision
+ *
+ *
+ */
+
+#include <ptlib.h>
+
+#include "opalutils.h"
+
+#define new PNEW
+
+/////////////////////////////////////////////////////////////////////////////
+PString GetPartyName(const PString & party)
+{
+  PINDEX beg = party.Find(':');
+
+  if (beg != P_MAX_INDEX)
+    beg++;
+
+  PINDEX end = party.Find('@');
+
+  if (end != P_MAX_INDEX)
+    end--;
+
+  return party(beg, end);
+}
+/////////////////////////////////////////////////////////////////////////////
+
---
+++ opal/opalutils.h
@@ -0,0 +1,42 @@
+/*
+ * opalutils.h
+ *
+ * T38FAX Pseudo Modem
+ *
+ * Copyright (c) 2007 Vyacheslav Frolov
+ *
+ * Open H323 Project
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open H323 Library.
+ *
+ * The Initial Developer of the Original Code is Vyacheslav Frolov
+ *
+ * Contributor(s):
+ *
+ * $Log: opalutils.h,v $
+ * Revision 1.1  2007-07-20 14:26:40  vfrolov
+ * Initial revision
+ *
+ * Revision 1.1  2007/07/20 14:26:40  vfrolov
+ * Initial revision
+ *
+ *
+ */
+
+#ifndef _OPALUTILS_H
+#define _OPALUTILS_H
+
+extern PString GetPartyName(const PString & party);
+
+#endif  // _OPALUTILS_H
+
